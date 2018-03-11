0c33f416486c4fc5876d7e2e9103780bb5813ea2
Juggernaut01 <erikas.ustila@gmail.com>
2018-03-11 13:11:02 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+<html>
+<style>
+body {
+        background-image: url("https://scontent.fvno2-1.fna.fbcdn.net/v/t1.0-9/17796417_1297859820261197_7051663520262929095_n.jpg?oh=6380038ff83e0399f37bc7a919339ff4&oe=5B06E3DC");
+} 
+ 
+</style>
+
+</html>
+
+?>
